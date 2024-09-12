aa07ae9a69b594389062033108c9d58513ca684b
ShouryaGoje <shouryagoje@gmail.com>
2024-09-12 16:43:03 +0530

    delete user api added

--- api/views.py
+++ api/views.py
@@ -61,40 +61,6 @@ class LogoutView(APIView):
         response.status_code = 200
         return response
     
-
-
-class CafeInfoView(APIView):
-    serializer_class = CafeInfoSerializer
-    def post(self, request, foramt = None):
-            
-        token = request.COOKIES.get('jwt')
-
-        if not token:
-            return Response({"error":"Unauthenticated User"},status=status.HTTP_400_BAD_REQUEST)
-        try:
-            payload = jwt.decode(token, 'secret', algorithms=['HS256'])
-
-        except jwt.ExpiredSignatureError as e:
-            return Response({"error":f"{e}"},status=status.HTTP_400_BAD_REQUEST)
-
-        user = User.objects.filter(id=payload['id']).first()
-        serializer = self.serializer_class(data = request.data)
-        if serializer.is_valid():
-            Cafe_Name = serializer.validated_data['Cafe_Name']
-            Cafe_Address = serializer.validated_data['Cafe_Address']
-            Cafe_Contact = serializer.validated_data['Cafe_Contact']
-            Owner_Name = serializer.validated_data['Owner_Name']
-            Owner_Contact = serializer.validated_data['Owner_Contact']
-
-            try:
-                user = User.objects.get(id=user.id)
-                cafe_info = Cafe_Info(user=user, Cafe_Address = Cafe_Address, Cafe_Name = Cafe_Name, Cafe_Contact = Cafe_Contact, Owner_Name = Owner_Name, Owner_Contact = Owner_Contact)
-                cafe_info.save()
-                return Response({"success":"User Info saved"},status=status.HTTP_200_OK)
-            except Exception as e:
-                return Response({"error":""},status=status.HTTP_400_BAD_REQUEST)
-        return Response({"error":"Bad body parameters"},status=status.HTTP_424_FAILED_DEPENDENCY)
-    
 class DeleteUser(APIView):
    
     def post(self, request, foramt = None):
