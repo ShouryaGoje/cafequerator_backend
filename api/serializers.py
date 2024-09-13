938f4c2729e9b81af70e54ae279dd5e71cca93d4
ShouryaGoje <shouryagoje@gmail.com>
2024-09-13 15:56:27 +0530

    access_token api added

--- api/serializers.py
+++ api/serializers.py
@@ -54,7 +54,7 @@ class CombinedUserCafeSerializer(serializers.ModelSerializer):
     
 class SpotifyParameterSerializer(serializers.ModelSerializer):
     class Meta:
-        models = Spotify_Api_Parameters
+        model = Spotify_Api_Parameters
         fields = {"access_token","refresh_token","expires_at"}
 
 
