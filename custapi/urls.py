e6edcbbe478d1de70eb8489c5fea2c12db5a88be
ShouryaGoje <shouryagoje@gmail.com>
2024-10-15 01:08:53 +0530

    cust api start get access token

--- custapi/urls.py
+++ custapi/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('login', LoginView.as_view()),
+    path('access-token', GetAccessToken.as_view()),
 ]
--- custapi/views.py
+++ custapi/views.py
@@ -2,7 +2,8 @@ from rest_framework.views import APIView, status
 from rest_framework.response import Response
 from rest_framework.exceptions import AuthenticationFailed
 from .serializers import *
-from .models import *
+from api.serializers import SpotifyParameterSerializer
+from api.models import *
 import jwt, datetime
 from datetime import datetime, timedelta, timezone
 
@@ -33,5 +34,54 @@ class LoginView(APIView):
 
             response.status_code = 200
             return response
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
 
+
+class GetAccessToken(APIView):
+    def get(self,request):
+     
+        auth_header = request.headers.get('Authorization')
+        if not auth_header or not auth_header.startswith('Bearer '):
+            return Response({"error": "Authorization header missing or improperly formatted"}, status=status.HTTP_401_UNAUTHORIZED)
+
+        # Extract the token
+        token = auth_header.split(' ')[1]
+
+        # Check if the token exists
+        if not token:
+            return Response({"error": "JWT token missing from cookies"}, status=status.HTTP_401_UNAUTHORIZED)
+
+        try:
+            # Decode the JWT token to get the payload
+            payload = jwt.decode(token, 'secret', algorithms=['HS256'])
+        except jwt.ExpiredSignatureError as e:
+            return Response({"error": f"Token expired: {e}"}, status=status.HTTP_400_BAD_REQUEST)
+        except jwt.InvalidTokenError as e:
+            return Response({"error": f"Invalid token: {e}"}, status=status.HTTP_400_BAD_REQUEST)
+        
+        if payload['auth']!= 'Cust':
+            return Response({"error": "API Access not authorized"}, status=status.HTTP_401_UNAUTHORIZED)
+
+        # Find the user from the payload
+        user = User.objects.filter(id=payload['id']).first()
+        if user is None:
+            return Response({"error":"User Not Found"},status=status.HTTP_400_BAD_REQUEST)
+        
+        token_info = Spotify_Api_Parameters.objects.filter(user=user).first()
+        response = Response()
+        
+
+       
+        access_token = SpotifyParameterSerializer(token_info).data["access_token"]
+        if access_token == '':
+            access_token = "Not set"
+        
+
+        response.data = {
+            "access_token": access_token
+        }
+
+        response.status_code = 200
+        return response
+    
 
